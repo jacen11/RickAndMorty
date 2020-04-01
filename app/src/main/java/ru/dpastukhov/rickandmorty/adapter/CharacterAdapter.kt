d3498757001bd195b76cd66f3c961b0434ab2d73
jacen <jacen19931@gmail.com>
2020-04-01 03:11:39 +0300

    Добавлена инфа во вьюшку рв

--- app/src/main/java/ru/dpastukhov/rickandmorty/adapter/CharacterAdapter.kt
+++ app/src/main/java/ru/dpastukhov/rickandmorty/adapter/CharacterAdapter.kt
@@ -3,8 +3,10 @@ package ru.dpastukhov.rickandmorty.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ru.dpastukhov.rickandmorty.R
 import ru.dpastukhov.rickandmorty.data.model.Character
 
@@ -12,6 +14,9 @@ class CharacterAdapter(private val characters: List<Character?>) : RecyclerView.
 
     class CharacterHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var name: TextView = itemView.findViewById(R.id.txtName)
+        var status: TextView = itemView.findViewById(R.id.txtStatus)
+        var origin: TextView = itemView.findViewById(R.id.txtOrigin)
+        var image: ImageView = itemView.findViewById(R.id.img)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterHolder {
@@ -28,5 +33,8 @@ class CharacterAdapter(private val characters: List<Character?>) : RecyclerView.
 
     override fun onBindViewHolder(holder: CharacterHolder, position: Int) {
         holder.name.text = characters[position]?.name
+        holder.status.text = characters[position]?.status
+        holder.origin.text = characters[position]?.origin?.name
+        Glide.with(holder.image).load(characters[position]?.image).into(holder.image);
     }
 }
